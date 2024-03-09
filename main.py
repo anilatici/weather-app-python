1909f7564695d3c533e867c9daea24ed80c856fb
Nebi Anil Atici <anillatici@gmail.com>
2024-03-09 14:09:33 +0000

    initialize api key

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
 import tkinter as tk
 from tkinter import messagebox
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+print(API_KEY)
