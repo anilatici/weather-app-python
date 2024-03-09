d8e2a264124a34f99c041f8f14f5c60a71c152e8
Nebi Anil Atici <anillatici@gmail.com>
2024-03-09 14:57:54 +0000

    add weather data for london

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ import tkinter as tk
 from tkinter import messagebox
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
+
+# load env file
 load_dotenv()
-
 API_KEY = os.getenv("API_KEY")
+
+# get CURRENT weather data
+url = "http://api.weatherapi.com/v1/current.json?key=" + API_KEY + "&q=London" + "&aqi=yes"
+response = requests.get(url)
+
+st.write("Weather App")
+st.write("This app is built using Streamlit and Python")
+st.write("City: " + response.json()["location"]["name"])
+st.write("Region: " + response.json()["location"]["region"])
+st.write("Country: " + response.json()["location"]["country"])
+st.write("Local Time: " + response.json()["location"]["localtime"])
+st.write("Temperature: " + str(response.json()["current"]["temp_c"]) + "°C")
+
+
