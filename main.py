def97bda62f218798bcce34465c7b0e8b123c0c7
Nebi Anil Atici <anillatici@gmail.com>
2024-03-09 15:16:54 +0000

    remove countries from main

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from tkinter import messagebox
 from dotenv import load_dotenv
 import os
 import streamlit as st
+import json
 
 
 # load env file
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+
 
 # get CURRENT weather data
 url = "http://api.weatherapi.com/v1/current.json?key=" + API_KEY + "&q=London" + "&aqi=yes"
@@ -16,10 +18,3 @@ response = requests.get(url)
 
 st.write("Weather App")
 st.write("This app is built using Streamlit and Python")
-st.write("City: " + response.json()["location"]["name"])
-st.write("Region: " + response.json()["location"]["region"])
-st.write("Country: " + response.json()["location"]["country"])
-st.write("Local Time: " + response.json()["location"]["localtime"])
-st.write("Temperature: " + str(response.json()["current"]["temp_c"]) + "°C")
-
-
